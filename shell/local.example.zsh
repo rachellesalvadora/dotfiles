459d22c12d6fd819c852670a687ce6646e20b873
Rachelle Salvadora <rachellesalvadora@gmail.com>
2026-05-13 15:52:48 +1000

    Strip local.example.zsh to bare section headers
    Let users define their own aliases and config instead of
    shipping generic examples they'll never use.

--- shell/local.example.zsh
+++ shell/local.example.zsh
@@ -4,46 +4,15 @@
 # ---------------------------------------------------------------------------
 # Editor
 # ---------------------------------------------------------------------------
-# export EDITOR="code --wait"
-# export VISUAL="$EDITOR"
 
 # ---------------------------------------------------------------------------
 # Extra paths
 # ---------------------------------------------------------------------------
-# export PATH="$HOME/.local/bin:$PATH"
-# export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # ---------------------------------------------------------------------------
-# Project navigation
+# Aliases
 # ---------------------------------------------------------------------------
-# alias work='cd ~/projects/my-app'
-# alias infra='cd ~/projects/infrastructure'
 
 # ---------------------------------------------------------------------------
-# Docker / container aliases
+# Environment variables
 # ---------------------------------------------------------------------------
-# alias dc='docker compose'
-# alias dce='docker compose exec'
-# alias dcea='docker compose exec -T app'
-# alias dcu='docker compose up -d'
-# alias dcd='docker compose down'
-
-# ---------------------------------------------------------------------------
-# Language tooling
-# ---------------------------------------------------------------------------
-# alias art='docker compose exec -T app php artisan'
-# alias tinker='docker compose exec -T app php artisan tinker'
-# alias phpunit='docker compose exec -T app vendor/bin/phpunit'
-# alias phpstan='docker compose exec -T app vendor/bin/phpstan analyse'
-
-# ---------------------------------------------------------------------------
-# Database
-# ---------------------------------------------------------------------------
-# alias migrate='docker compose exec -T app php artisan migrate'
-# alias rollback='docker compose exec -T app php artisan migrate:rollback'
-# alias seed='docker compose exec -T app php artisan db:seed'
-
-# ---------------------------------------------------------------------------
-# Misc
-# ---------------------------------------------------------------------------
-# alias reload='source ~/.zshrc'
